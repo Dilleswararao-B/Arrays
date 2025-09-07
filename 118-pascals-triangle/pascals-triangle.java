16c4ee6ed492b5350c2951c9634cc808a288a314
Dilleswararao-B <dilleswararao778@gmail.com>
2025-09-07 11:41:04 +0530

    Time: 1 ms (86.51%) | Memory: 41.7 MB (96.43%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+      int n = numRows;
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int row = 0; row < n; row++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int col = 0; col <= row; col++) {
+                temp.add(ncr(row, col));
+            }
+            ans.add(temp);
+        }
+        return ans;
+    }
+
+    public static int ncr(int n, int r) {
+        long res = 1;
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return (int) res;
+    }
+}
